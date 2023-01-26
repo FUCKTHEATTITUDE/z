74eb9ba670a632c8a83e4706ff19e7df1fcca53e
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-01-26 19:30:14 +0530

    Update Start.py

--- Start.py
+++ Start.py
@@ -10,12 +10,9 @@ import configparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+
 
 
 from cgitb import reset
